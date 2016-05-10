bosch-io/sw360portal

commit f8b1526d735a4de129788d62c737eff5d880f694

refactored license shortnames to ids and removed mainLicenseNames from releases and components

backend/src/src-licenses/src/main/java/com/siemens/sw360/licenses/db/LicenseDatabaseHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright Siemens AG, 2013-2015. Part of the SW360 Portal Project.
+ * Copyright Siemens AG, 2013-2016. Part of the SW360 Portal Project.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License Version 2.0 as published by the
@@ -375,7 +375,7 @@ public class LicenseDatabaseHandler {
 
             dbLicense = updateLicenseFromInputLicense(dbLicense, inputLicense, businessUnit);
 
-            if(dbLicense.isSetId()) {
+            if(inputLicense.isSetId()) {
                 licenseRepository.update(dbLicense);
             } else {
                 licenseRepository.add(dbLicense);
@@ -411,7 +411,8 @@ public class LicenseDatabaseHandler {
         }
         license.setText(inputLicense.getText());
         license.setFullname(inputLicense.getFullname());
-        license.setShortname(inputLicense.getShortname());
+        license.setId(inputLicense.getShortname());
+        license.unsetShortname();
         license.setLicenseTypeDatabaseId(inputLicense.getLicenseTypeDatabaseId());
         license.unsetLicenseType();
         license.setGPLv2Compat(inputLicense.GPLv2Compat);

backend/src-common/src/main/java/com/siemens/sw360/components/summary/LicenseSummary.java
@@ -1,5 +1,5 @@
 /*
- * Copyright Siemens AG, 2014-2015. Part of the SW360 Portal Project.
+ * Copyright Siemens AG, 2014-2016. Part of the SW360 Portal Project.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License Version 2.0 as published by the
@@ -43,7 +43,6 @@ public class LicenseSummary extends DocumentSummary<License> {
                 copyField(document, copy, _Fields.LICENSE_TYPE);
             default:
                 copyField(document, copy, _Fields.ID);
-                copyField(document, copy, _Fields.SHORTNAME);
                 copyField(document, copy, _Fields.FULLNAME);
                 copyField(document, copy, _Fields.LICENSE_TYPE_DATABASE_ID);
         }

backend/src-common/src/main/java/com/siemens/sw360/datahandler/entitlement/ComponentModerator.java
@@ -79,7 +79,6 @@ public class ComponentModerator extends Moderator<Component._Fields, Component> 
                 case RELEASES:
                 case RELEASE_IDS:
                 case MAIN_LICENSE_IDS:
-                case MAIN_LICENSE_NAMES:
                 case LANGUAGES:
                 case OPERATING_SYSTEMS:
                 case VENDOR_NAMES:

backend/src/src-licenses/src/main/java/com/siemens/sw360/licenses/db/LicenseRepository.java
@@ -1,5 +1,5 @@
 /*
- * Copyright Siemens AG, 2013-2015. Part of the SW360 Portal Project.
+ * Copyright Siemens AG, 2013-2016. Part of the SW360 Portal Project.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License Version 2.0 as published by the
@@ -35,7 +35,7 @@ import java.util.List;
 public class LicenseRepository extends SummaryAwareRepository<License> {
 
     private static final String BY_NAME_VIEW = "function(doc) { if(doc.type == 'license') { emit(doc.fullname, doc) } }";
-    private static final String BY_SHORT_NAME_VIEW = "function(doc) { if(doc.type == 'license') { emit(doc.shortname, doc) } }";
+    private static final String BY_SHORT_NAME_VIEW = "function(doc) { if(doc.type == 'license') { emit(doc._id, doc) } }";
 
     public LicenseRepository(DatabaseConnector db) {
         super(License.class, db, new LicenseSummary());

backend/src/src-licenses/src/test/java/com/siemens/sw360/licenses/LicenseHandlerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright Siemens AG, 2013-2015. Part of the SW360 Portal Project.
+ * Copyright Siemens AG, 2013-2016. Part of the SW360 Portal Project.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License Version 2.0 as published by the
@@ -90,9 +90,8 @@ public class LicenseHandlerTest {
         assertEquals(summary.size(), licenses.size());
 
         for (License license : summary) {
-            // Only those three fields should be present
+            // Only those two fields should be present
             assertNotNull(license.id);
-            assertNotNull(license.shortname);
             assertNotNull(license.fullname);
             // The rest should not be set
             assertFalse(license.isSetTodoDatabaseIds());
@@ -108,7 +107,6 @@ public class LicenseHandlerTest {
 
         assertEquals(expLicense.getId(), actLicense.getId());
         assertEquals(expLicense.getFullname(), actLicense.getFullname());
-        assertEquals(expLicense.getShortname(), actLicense.getShortname());
         assertEquals(expLicense.getTodoDatabaseIdsSize(), actLicense.getTodosSize());
 
         // Check obligations
@@ -151,8 +149,7 @@ public class LicenseHandlerTest {
         obligations = new HashMap<>();
 
         License license1 = new License();
-        license1.setId("L1");
-        license1.setShortname("Apache 1.1");
+        license1.setId("Apache 1.1");
         license1.setFullname("The Apache Software License, Version 1.1");
         license1.setLicenseType(new LicenseType().setLicenseTypeId(3).setType("Red - copyleft effect"));
         license1.addToRisks(new Risk().setRiskId(123123).setText("If Siemens uses this contractor pattern a long text follows here for reading and display... this might be long.").setCategory(new RiskCategory().setRiskCategoryId(32).setText("Beige")));
@@ -161,16 +158,15 @@ public class LicenseHandlerTest {
         license1.addToTodoDatabaseIds("T1");
         license1.addToTodoDatabaseIds("T2");
         license1.addToTodoDatabaseIds("T5");
-        licenses.put(license1.shortname, license1);
+        licenses.put(license1.id, license1);
 
         License license2 = new License();
-        license2.setId("L2");
-        license2.setShortname("Apache 2.0");
+        license2.setId("Apache 2.0");
         license2.setFullname("The Apache Software License, Version 2.0");
         license2.setReviewdate("12.12.2012");
         license2.addToTodoDatabaseIds("T3");
         license2.addToTodoDatabaseIds("T4");
-        licenses.put(license2.shortname, license2);
+        licenses.put(license2.id, license2);
 
         Todo todo1 = new Todo().setId("T1").setText("You must include the acknowledgement as part of the documentation for the end user. An example looks as following:  This product includes software developed by the Apache Software Foundation (http://www.apache.org/).");
         todo1.addToObligationDatabaseIds("O1");

backend/src/src-moderation/src/main/java/com/siemens/sw360/moderation/db/ComponentModerationRequestGenerator.java
@@ -64,7 +64,6 @@ public class ComponentModerationRequestGenerator extends ModerationRequestGenera
                     case RELEASES:
                     case RELEASE_IDS:
                     case MAIN_LICENSE_IDS:
-                    case MAIN_LICENSE_NAMES:
                     case LANGUAGES:
                     case OPERATING_SYSTEMS:
                     case VENDOR_NAMES:

frontend/sw360-portlet/src/main/java/com/siemens/sw360/portal/common/PortalConstants.java
@@ -1,5 +1,5 @@
 /*
- * Copyright Siemens AG, 2013-2015. Part of the SW360 Portal Project.
+ * Copyright Siemens AG, 2013-2016. Part of the SW360 Portal Project.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License Version 2.0 as published by the
@@ -38,6 +38,7 @@ public class PortalConstants {
     public static final String SOFTWARE_PLATFORMS;
     public static final String OPERATING_SYSTEMS;
     public static final Set<String> SET_CLEARING_TEAMS_STRING;
+    public static final String LICENSE_IDENTIFIERS;
 
     //! Role names
     public static final String ROLENAME_ADMIN = "Administrator";
@@ -237,6 +238,7 @@ public class PortalConstants {
         SET_CLEARING_TEAMS_STRING = CommonUtils.splitToSet(props.getProperty("clearing.teams", "org1,org2,org3"));
         STATE = props.getProperty("state","[ \"Active\", \"Phase out\", \"Unknown\"]");
         PROJECT_TYPE = props.getProperty("project.type","[ \"Customer Project\", \"Internal Project\", \"Product\", \"Service\"]");
+        LICENSE_IDENTIFIERS = props.getProperty("license.identifiers", "[]");
     }
 
     private PortalConstants() {

frontend/sw360-portlet/src/main/java/com/siemens/sw360/portal/portlets/components/ComponentPortlet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright Siemens AG, 2013-2015. Part of the SW360 Portal Project.
+ * Copyright Siemens AG, 2013-2016. Part of the SW360 Portal Project.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License Version 2.0 as published by the
@@ -473,8 +473,6 @@ public class ComponentPortlet extends FossologyAwarePortlet {
             try {
                 ComponentService.Iface client = thriftClients.makeComponentClient();
                 Component component = client.getComponentById(id, user);
-                final Set<String> licenseNames = new HashSet<>(SW360Utils.getLicenseNamesFromIds(component.getMainLicenseIds(), user.getDepartment()));
-                component.setMainLicenseNames(licenseNames);
 
                 request.setAttribute(COMPONENT, component);
                 request.setAttribute(DOCUMENT_ID, id);
@@ -525,8 +523,6 @@ public class ComponentPortlet extends FossologyAwarePortlet {
 
             if (!isNullOrEmpty(releaseId)) {
                 release = client.getReleaseById(releaseId, user);
-                final Set<String> licenseNames = new HashSet<>(SW360Utils.getLicenseNamesFromIds(release.getMainLicenseIds(), user.getDepartment()));
-                release.setMainLicenseNames(licenseNames);
 
                 request.setAttribute(RELEASE_ID, releaseId);
                 request.setAttribute(RELEASE, release);

commit f1c2b19c7cb06226685c39447e1224132b16b8dc

Fix License Moderation Requests
bug was introduced by f8b1526d735a4de129788d62c737eff5d880f694

solution suggested by @heydenreich

backend/src/src-licenses/src/main/java/com/siemens/sw360/licenses/db/LicenseDatabaseHandler.java
@@ -371,7 +371,10 @@ public class LicenseDatabaseHandler {
 
             License dbLicense = new License();
             if(inputLicense.isSetId()) {
-                dbLicense = licenseRepository.get(inputLicense.getId());
+                License dbLicenseTmp = licenseRepository.get(inputLicense.getId());
+                if (dbLicenseTmp != null){
+                    dbLicense = dbLicenseTmp;
+                }
             }
 
             dbLicense = updateLicenseFromInputLicense(dbLicense, inputLicense, businessUnit);
@@ -412,7 +415,7 @@ public class LicenseDatabaseHandler {
         }
         license.setText(inputLicense.getText());
         license.setFullname(inputLicense.getFullname());
-        license.setId(inputLicense.getId());
+        license.setId(inputLicense.isSetId() ? inputLicense.getId() : inputLicense.getShortname());
         license.unsetShortname();
         license.setLicenseTypeDatabaseId(inputLicense.getLicenseTypeDatabaseId());
         license.unsetLicenseType();